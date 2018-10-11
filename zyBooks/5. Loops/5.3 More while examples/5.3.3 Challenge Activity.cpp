37dc6ae22de4d14dd0dca4ff902da8b59817bb9b
Tikichief <ch1ck3n09@gmail.com>
2018-10-11 13:35:38 -0400

    added 5.3.3 Challenge Activty

---
+++ zyBooks/5. Loops/5.3 More while examples/5.3.3 Challenge Activity.cpp
@@ -0,0 +1,21 @@
+/*
+5.3.3: While loop: Insect growth.
+Given positive integer numInsects, write a while loop that prints that number doubled without reaching 100. 
+Follow each number with a space. After the loop, print a newline. Ex: If numInsects = 8, print:
+8 16 32 64
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int numInsects;
+
+   cin >> numInsects; // Must be >= 1
+
+   /* Your solution goes here  */
+
+   system("pause");
+   return 0;
+}
