e75c43a35368aaa57d0c445217ec975e8392d3ef
Tikichief <ch1ck3n09@gmail.com>
2018-10-17 12:10:29 -0400

    Created week 8 lab

---
+++ CompSci Labs/Week 8 Lab.cpp
@@ -0,0 +1,38 @@
+/*
+1.	Write a program demonstrates the ++ and −− operators. Use prefix and postfix mode for same variable  
+This is my variable = 7
+your output should look like the example.
+Example is 
+Now the variable num is 5 
+I will increment num again. 
+Now the variable num is 6 
+I will now decrement num. 
+Now the variable num is 5 
+I will decrement num again. 
+Now the variable num is 4
+*/
+
+/*
+2.	What will the following program segments display?
+A)	x = 2; 
+y = x++; 
+cout << x << y; 
+B)	x = 2; 
+y = ++x; 
+cout << x << y; 
+C)	x = 2; 
+y = 4; 
+cout << x++ << −−y; 
+D)	x = 2; 
+y = 2 * x++; 
+cout << x << y; 
+E)	x = 99; 
+if (x++ < 100) {
+cout "It is true!\n"; 
+else 
+cout << "It is false!\n";}
+F)	x = 0; if (++x) {
+cout << "It is true!\n"; 
+else 
+cout << "It is false!\n";}
+*/
