f384b556d8ba4f7f1eb94249cd3778fdaea47a5f
Tikichief <ch1ck3n09@gmail.com>
2018-10-17 12:29:47 -0400

    Created week 9-2 lab

---
+++ CompSci Labs/Week 9-2 Lab.cpp
@@ -0,0 +1,9 @@
+/*
+Pennies for Pay
+Write a program that calculates how much a person would earn over a period of time if his or her salary 
+is one penny the first day and two pennies the second day, and continues to double each day. 
+The program should ask the user for the number of days. The output should be displayed in a dollar amount, not the number of pennies.   
+Input Validation: Do not accept a number less than 1 for the number of days worked.
+*/
+
+
