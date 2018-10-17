6689b344d5b739e97e48187cf7277d3a316fe00b
Tikichief <ch1ck3n09@gmail.com>
2018-10-17 11:24:10 -0400

    Added week 2 lab

---
+++ CompSci Labs/Week 2 Lab.cpp
@@ -0,0 +1,26 @@
+/*
+Lab - 2
+On paper, write a program that will display your name on the first line, your street address on the second line, 
+your city, state, and Zip code on the third line, and your telephone number on the fourth line. 
+Place a comment with today’s date at the top of the program. Test your program by entering, compiling, and running it.
+22 Aug 2019
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main();
+{
+  cout << "Name: Kareem Gourdine";
+  cout << endl;
+  cout << "Adress: 1234 Chicken Dr";
+  cout << endl;
+  cout << "City/State: Washington, DC Zipcode: 22012";
+  cout << endl;
+  cout << "Telephone number: (281) 330-8004";
+  cout << endl;
+  
+  system("pause");
+  reutrn 0;
+}
