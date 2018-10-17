129141edb5d188e4e63b1bdbff4101197bc0debf
Tikichief <ch1ck3n09@gmail.com>
2018-10-17 11:31:32 -0400

    Created week 5 lab

---
+++ CompSci Labs/Week Lab.cpp
@@ -0,0 +1,11 @@
+/*
+Test Average
+Write a program that asks for five test scores. The program should calculate the average test score and display it. 
+If student’s grade is greater than or equal to 90
+Print “A”
+Else
+Study more to get “A”
+1 )Show your work in a paper 
+*/
+
+
