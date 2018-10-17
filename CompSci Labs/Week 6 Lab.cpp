3fb2805deed57d55b286153d874dc2040775b144
Tikichief <ch1ck3n09@gmail.com>
2018-10-17 12:08:51 -0400

    Created week 6 lab

---
+++ CompSci Labs/Week 6 Lab.cpp
@@ -0,0 +1,9 @@
+/*
+Areas of Rectangles
+The area of a rectangle is the rectangle’s length times its width. 
+Write a program that asks for the length and width of two rectangles. 
+The program should tell the user which rectangle has the greater area, or if the areas are the same.
+First s how your work in a paper then write your code on Visual studio
+*/
+
+
