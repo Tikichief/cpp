85b566f9e69364f5c52552abeeaf31ad73bbd5dd
Tikichief <ch1ck3n09@gmail.com>
2018-10-29 13:47:53 -0400

    Create 6.3.3 Out-of-range index using an array.cpp

---
+++ zyBooks/6. Arrays - Vectors/6.3 Iterating through arrays/6.3.3 Out-of-range index using an array.cpp
@@ -0,0 +1,23 @@
+/*
+Common error: Accesing out of range array element
+A common error is to try to access an array with an index that is out of the array's index range. Ex: Trying to access highScores[8] when 
+highScores's valid indices are 0-7. Care should be taken whenever a user enters a number that is then used as an array index, and when 
+using a loop index as an array index also, to ensure the index is within the array's valid index range. Checking whether an array index is 
+in range is very important. Trying to access an array with an out-of-range index is not only a very common error, but is also one of the 
+hardest errors to debug. The following animation shows what happens when a program writes to an out-of-range index using an array.
+*/
+
+// 6.3.3: Writing to an out-of-range index using an array.
+
+
+int userWeights[3];
+int userAge;
+
+userAge = 44;
+
+userWeights[0] = 122;
+userWeights[1] = 119;
+userWeights[2] = 117;
+userWeights[3] = 199; // (Problematic)
+
+// Print userAge
