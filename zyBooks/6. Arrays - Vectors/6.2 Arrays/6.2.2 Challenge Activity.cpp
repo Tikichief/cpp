f7fba15760299c60749169cb9387345ad4816b3b
Tikichief <ch1ck3n09@gmail.com>
2018-10-29 13:36:16 -0400

    Created 6.2.2 Challenge Activity

---
+++ zyBooks/6. Arrays - Vectors/6.2 Arrays/6.2.2 Challenge Activity.cpp
@@ -0,0 +1,34 @@
+/*
+Write three statements to print the first three elements of array runTimes. Follow each statement with a newline. Ex: If runTime = 
+{800, 775, 790, 805, 808}, print:
+
+800 
+775 
+790
+
+Note: These activities may test code with different test values. This activity will perform two tests, the first with a 5-element array 
+(int runTimes[5]), the second with a 4-element array (int runTimes[4]). See "How to Use zyBooks". . 
+
+Also note: If the submitted code tries to access an invalid array element, such as runTime[9] for a 5-element array, the test may 
+generate strange results. Or the test may crash and report "Program end never reached", in which case the system doesn't print the 
+test case that caused the reported message. 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int runTimes[5];
+
+   // Populate array
+   runTimes[0] = 800;
+   runTimes[1] = 775;
+   runTimes[2] = 790;
+   runTimes[3] = 805;
+   runTimes[4] = 808;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
