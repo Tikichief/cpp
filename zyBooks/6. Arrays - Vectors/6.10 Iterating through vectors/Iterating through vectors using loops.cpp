dc475119b5f220471250b5639a96a54eab50d396
Tikichief <ch1ck3n09@gmail.com>
2018-11-02 16:48:35 -0400

    Update Iterating through vectors using loops.cpp

--- zyBooks/6. Arrays - Vectors/6.10 Iterating through vectors/Iterating through vectors using loops.cpp
+++ zyBooks/6. Arrays - Vectors/6.10 Iterating through vectors/Iterating through vectors using loops.cpp
@@ -10,3 +10,9 @@ numbered 0 to N - 1 rather than 1 to N, programmers commonly use this for loop s
 for (i = 0; i < myVector.size(); ++i) {
    // Loop body accessing myVector.at(i)
 }
+
+/*
+Note that index variable i is initialized to 0, and the loop expression is i < myVector.size() rather than i <= myVector.size(). 
+If myVector.size() were 5, the loop's iterations would set i to 0, 1, 2, 3, and 4, for a total of 5 iterations. The benefit of the loop
+structure is that each vector element is accessed as myVector.at(i) rather than the more complex myVector.at(i - 1).
+*/
