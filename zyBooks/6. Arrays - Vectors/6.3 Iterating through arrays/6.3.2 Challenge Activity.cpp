871c7d6fb3b05b8f2e95e6af713cab74bdd7282c
Tikichief <ch1ck3n09@gmail.com>
2018-11-02 15:30:33 -0400

    Create 6.3.2 Challenge Activity.cpp

---
+++ zyBooks/6. Arrays - Vectors/6.3 Iterating through arrays/6.3.2 Challenge Activity.cpp
@@ -0,0 +1,31 @@
+/*
+6.3.2: Finding values in arrays.
+
+Set numMatches to the number of elements in userValues (having NUM_VALS elements) that equal matchValue. 
+Ex: If matchValue = 2 and userValues = {2, 2, 1, 2}, then numMatches = 3. (Notes)
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   const int NUM_VALS = 4;
+   int userValues[NUM_VALS];
+   int i;
+   int matchValue;
+   int numMatches = -99; // Assign numMatches with 0 before your for loop
+
+   userValues[0] = 2;
+   userValues[1] = 2;
+   userValues[2] = 1;
+   userValues[3] = 2;
+
+   matchValue = 2;
+
+   /* Your solution goes here  */
+
+   cout << "matchValue: " << matchValue << ", numMatches: " << numMatches << endl;
+
+   return 0;
+}
