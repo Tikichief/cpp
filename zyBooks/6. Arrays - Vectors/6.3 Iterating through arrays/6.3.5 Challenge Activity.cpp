4c060ddbff4fe741ad7d5da676abba2b861917ee
Tikichief <ch1ck3n09@gmail.com>
2018-11-02 15:33:26 -0400

    Create 6.3.5 Challenge Activity.cpp

---
+++ zyBooks/6. Arrays - Vectors/6.3 Iterating through arrays/6.3.5 Challenge Activity.cpp
@@ -0,0 +1,31 @@
+/*
+6.3.5: Printing array elements separated by commas.
+
+Write a for loop to print all NUM_VALS elements of array hourlyTemp. Separate elements with a comma and space. 
+Ex: If hourlyTemp = {90, 92, 94, 95}, print:
+
+90, 92, 94, 95
+
+Your code's output should end with the last element, without a subsequent comma, space, or newline. 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   const int NUM_VALS = 4;
+   int hourlyTemp[NUM_VALS];
+   int i;
+
+   hourlyTemp[0] = 90;
+   hourlyTemp[1] = 92;
+   hourlyTemp[2] = 94;
+   hourlyTemp[3] = 95;
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
