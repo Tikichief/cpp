0d6df5dab3e4f110a17738321ab6fb79013b31bb
Tikichief <ch1ck3n09@gmail.com>
2018-11-02 16:47:52 -0400

    Create Iterating through vectors using loops.cpp

---
+++ zyBooks/6. Arrays - Vectors/6.10 Iterating through vectors/Iterating through vectors using loops.cpp
@@ -0,0 +1,12 @@
+/*
+Iterating through vectors using loops
+
+Iterating through vectors using loops is commonplace and is an important programming skill to master. Because vector indices are 
+numbered 0 to N - 1 rather than 1 to N, programmers commonly use this for loop structure:
+
+6.10.1: Common for loop structure for iterating through a vector.
+*/
+// Iterating through myVector
+for (i = 0; i < myVector.size(); ++i) {
+   // Loop body accessing myVector.at(i)
+}
