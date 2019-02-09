ca1526ded087a0a17449e4bc791d64c758a619f8
Tikichief <ch1ck3n09@gmail.com>
2019-02-09 00:10:28 -0500

    Create LinearEquationTest.cpp

---
+++ CompSci II Labs/0128/LinearEquationTest.cpp
@@ -0,0 +1,21 @@
+#include "LinearEquation.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	LinearEquation a, b(10, 5);
+	LinearEquation c(5, 10);
+
+	cout << (c + b) << endl;
+	cout << "y = " << a.getslope() << "x" << " + " << a.getintercept() << endl;
+	cout << "y = " << b.getslope() << "x" << " + " << b.getintercept() << endl;
+	cout << a.evaluate(5) << endl;
+	cout << b.evaluate(10) << endl;
+	
+	cout << a.approxIntegral(0, 15) << endl;
+
+
+	system("pause");
+	return 0;
+}
